fe85aa5e639795c34f0d69317dba4b627047a33e
JaySurya <jaysurya6969@gmail.com>
2025-07-19 13:48:57 +0530

    defined controls for the snake

--- snakegame.py
+++ snakegame.py
@@ -19,19 +19,33 @@ pygame.display.set_caption("Snake Game")
 #Gamevariables
 snake_x=45
 snake_y=45
-snake_size=10
+snake_size_x=10
+snake_size_y=10
 exit_game=False
 
 #Gameloop
 while not exit_game:
     gamewindow.fill(black)
-    pygame.display.update()
+    pygame.draw.rect(gamewindow,green,[snake_x,snake_y,snake_size_x,snake_size_y])
+    
     
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             exit_game=True
 
-    pygame.draw.rect(gamewindow,green,[snake_x,snake_y,snake_size,snake_size])
+        
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_RIGHT:
+                snake_x=snake_x+5
+            if event.key==pygame.K_LEFT:
+                snake_x=snake_x-5
+            if event.key==pygame.K_UP:
+                snake_y=snake_y-5
+            if event.key==pygame.K_DOWN:
+                snake_y=snake_y+5
+
+
+
     pygame.display.update()
 
 
