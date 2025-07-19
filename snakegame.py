0934c9dfeda41c68a050e81a4ae3f924a0755166
JaySurya <jaysurya6969@gmail.com>
2025-07-19 14:23:57 +0530

    continuos movement of the snake

--- snakegame.py
+++ snakegame.py
@@ -21,7 +21,11 @@ snake_x=45
 snake_y=45
 snake_size_x=10
 snake_size_y=10
+snake_velocity_x=5
+snake_velocity_y=5
 exit_game=False
+direction=None
+
 
 #Gameloop
 while not exit_game:
@@ -36,15 +40,32 @@ while not exit_game:
         
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_RIGHT:
-                snake_x=snake_x+5
+                # snake_x=snake_x+snake_velocity_x
+                direction=3     
             if event.key==pygame.K_LEFT:
-                snake_x=snake_x-5
+                # snake_x=snake_x-snake_velocity_x
+                direction=4
             if event.key==pygame.K_UP:
-                snake_y=snake_y-5
+                # snake_y=snake_y-snake_velocity_y
+                direction=1
             if event.key==pygame.K_DOWN:
-                snake_y=snake_y+5
+                # snake_y=snake_y+snake_velocity_y
+                direction=2
+
+    if(direction==1):
+        snake_y=snake_y-snake_velocity_y
+    elif(direction==2):
+        snake_y=snake_y+snake_velocity_y
+    elif(direction==3):
+        snake_x=snake_x+snake_velocity_x
+    elif(direction==4):
+        snake_x=snake_x-snake_velocity_x
+
+        
 
 
+
+        
 
     pygame.display.update()
 
