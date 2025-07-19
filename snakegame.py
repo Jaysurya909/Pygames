463ad9c3cdd87a0f4057004764a317f8544c0f79
JaySurya <jaysurya6969@gmail.com>
2025-07-19 13:30:35 +0530

    made the head of the snake

--- snakegame.py
+++ snakegame.py
@@ -3,6 +3,7 @@ import pygame
 #colors
 white=(255,255,255)
 black=(0,0,0)
+green=(0,255,0)
 
 #window size
 screen_width=700
@@ -11,13 +12,19 @@ gamewindow=pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Snake Game")
 
 #Gamevariables
+snake_x=45
+snake_y=45
+snake_size=10
 exit_game=False
 
 #Gameloop
 while not exit_game:
-    gamewindow.fill(white)
+    gamewindow.fill(black)
     pygame.display.update()
     
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             exit_game=True
+
+    pygame.draw.rect(gamewindow,green,[snake_x,snake_y,snake_size,snake_size])
+    pygame.display.update()
